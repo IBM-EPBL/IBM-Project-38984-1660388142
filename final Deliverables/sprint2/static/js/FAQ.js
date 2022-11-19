ab099fb4be38885ce409adc94b71b5d5b43dc965
Rishiragul <95639964+Rishiragul@users.noreply.github.com>
2022-11-20 00:34:07 +0530

    Delete final Deliverables/sprint2 directory

--- final Deliverables/sprint2/static/js/FAQ.js
+++
@@ -1,8 +0,0 @@
-
-    const question = document.querySelectorAll('.faq');
-
-    question.forEach(faq => {
-        faq.addEventListener("click", () => {
-            faq.classList.toggle("active");
-        })
-    })
